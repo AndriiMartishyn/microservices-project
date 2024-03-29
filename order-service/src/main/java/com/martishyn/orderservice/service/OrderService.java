2554e132b02dd2d1d6ab90db7fec3fb85d9a1b60
Andrii Martishyn <contactgently@gmail.com>
2024-03-29 14:59:27 +0200

    added balancing and replaced hardcoded ip address of inventory service for webclient call from order with context path

--- order-service/src/main/java/com/martishyn/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/martishyn/orderservice/service/OrderService.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -34,8 +34,8 @@ public class OrderService {
                 .stream()
                 .map(OrderLineItems::getSkuCode).toList();
 
-        InventoryResponse[] inventoryResponseArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
                 .block();
